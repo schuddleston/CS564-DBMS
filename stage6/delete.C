b580f617f44bf8d1059abf07f5c6053564b6e240
kramesh-wisc-git <kramesh5@wisc.edu>
2024-12-12 16:18:45 -0600

    added comments to delete.c

--- stage6/delete.C
+++ stage6/delete.C
@@ -20,18 +20,19 @@ const Status QU_Delete(const string & relation,
 
 	Status status = OK;
 	HeapFileScan scanner(relation, status);
-	if(status != OK) return status;
+	if(status != OK) return status; // error from creating scanner object instance
 
+	// start scanning for appropriate entires in the table that match parameters
 	status = scanner.startScan(0, attrName.length(), type, attrValue, op);
-	if(status != OK) return status;
+	if(status != OK) return status; // error from calling startScan()
 
 	RID rid;
 	while(scanner.scanNext(rid) == OK) {
-		status = scanner.deleteRecord();
-		if(status != OK) return status;
+		status = scanner.deleteRecord(); // delete the appropriate record
+		if(status != OK) return status; // error from calling deleteRecord()
 	}
 
-	return OK;
+	return OK; // success
 }
 
 
